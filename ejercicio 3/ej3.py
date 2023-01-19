5adb57110e83787130aafc65a2d3bd523634b717
crltsnch <crltsnch@myuax.com>
2023-01-19 23:33:33 +0100

    Ej 3. funcion sort by date

--- ejercicio 3/ej3.py
+++ ejercicio 3/ej3.py
@@ -9,3 +9,11 @@ class ArtefactosValiosos:
     def __str__(self):
         return 'Nombre: ' + self.nombre + ' Peso: ' + str(self.peso) + ' Precio: ' + str(self.precio) + ' Fecha de caducidad: ' + str(self.fechaCaducidad)
 
+def sortByDate(elem):
+    return datetime.strptime(elem.fechaCaducidad, '%d/%m/%Y')
+
+lsita = [ArtefactosValiosos('Diamante', 50, 500, '08/31/2024'),
+ArtefactosValiosos('Esmeralda', 50, 500, '07/28/2023'),
+ArtefactosValiosos('Perla', 50, 500, '09/30/2025')]
+
+
