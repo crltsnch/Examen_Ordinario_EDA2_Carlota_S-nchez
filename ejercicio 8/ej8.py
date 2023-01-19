311be2219bb7e12d04450f5cb2a8a5f4b5c2d50c
crltsnch <crltsnch@myuax.com>
2023-01-19 21:55:59 +0100

    cambios

--- ejercicio 8/ej8.py
+++ ejercicio 8/ej8.py
@@ -1,16 +1,29 @@
 import networkx as nx
 import random
 
-grafo = nx.Graph()   #grafo vacío
+g = nx.Graph()   #grafo vacío
 planetas = ['Alderaan', 'Endor', 'Dagobah', 'Hoth', 'Tatooine', 'Kamino', 'Naboo', 'Mustafar', 'Scarif', 'Bespin', 'Cantonica', 'DQar', 'Dantooine', 'Atollon', 'Jedha', 'Corellia', 'Coruscant']
-grafo.add_nodes_from(planetas)
 
-#agregamos 4 aristar por cada planeta
-def agregar_aristas(grafo, planetas):
-    for planeta in planetas:
-        for i in range(4):
-            planeta_aleatorio = random.choice([p for p in planetas if p != planeta])
-            grafo.add_edge(planeta, planeta_aleatorio)
-    return grafo
+g.add_node('Alderaan')
+g.add_node('Endor')
+g.add_node('Dagobah')
+g.add_node('Hoth')
+g.add_node('Tatooine')
+g.add_node('Kamino')
+g.add_node('Naboo')
+g.add_node('Mustafar')
+g.add_node('Scarif')
+g.add_node('Bespin')
+g.add_node('Cantonica')
+g.add_node('DQar')
+g.add_node('Dantooine')
+g.add_node('Atollon')
+g.add_node('Jedha')
+g.add_node('Corellia')
+g.add_node('Coruscant')
 
-#algoritmo de Kruskal
+#generamso al menos 4 aristas
+for i in range(4):
+    other_node = random.coice(list(g.nodes()))   #elegimos al azor otro nodo (planeta)
+    while other_node == node:
+        other_node = random.choice(list(g.nodes()))
