76e6cd8429a48085beee060130b65e3f0c8ba5f3
crltsnch <crltsnch@myuax.com>
2023-01-19 19:47:24 +0100

    Ej 7. constructor

--- ejercicio 7/ejercicio 7.py
+++ ejercicio 7/ejercicio 7.py
@@ -1,1 +1,9 @@
-import heapq
+class NodoArbol:
+    def __init__(self, simbolo, freq):
+        self.simbolo = simbolo
+        self.freq = freq
+        self.izq = None
+        self.der = None
+        self.padre = None
+
+
