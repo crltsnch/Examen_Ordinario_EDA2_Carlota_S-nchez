8c2fd93ca4bc2df504fdd33ab3ca0b02b1440d96
crltsnch <crltsnch@myuax.com>
2023-01-19 19:58:04 +0100

    Ej 7. funcion crear arbol

--- ejercicio 7/ejercicio 7.py
+++ ejercicio 7/ejercicio 7.py
@@ -12,5 +12,14 @@ def ordenar_nodos(lista_nodos):
     return lista_nodos
 
 def insertar_nodo(lista_nodos, nodo):
-    for i in rang
+    for i in range(len(lista_nodos)):
+        if nodo.freq < lista_nodos[i].freq:
+            lista_nodos.insert(i, nodo)
+        if i == len(lista_nodos) - 1:
+            lista_nodos.append(nodo)
+            break
 
+    return lista_nodos
+
+def crear_arbol(lista_nodos):
+    
