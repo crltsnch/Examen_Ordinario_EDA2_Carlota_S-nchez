38a605437bbbd651e82da45f39b9d445c5c03c3b
crltsnch <crltsnch@myuax.com>
2023-01-19 21:44:10 +0100

    Ej 8. cuatro aristas por planeta

--- ejercicio 8/ej8.py
+++ ejercicio 8/ej8.py
@@ -1,11 +1,16 @@
 import networkx as nx
+import random
 
 grafo = nx.Graph()   #grafo vacío
 planetas = ['Alderaan', 'Endor', 'Dagobah', 'Hoth', 'Tatooine', 'Kamino', 'Naboo', 'Mustafar', 'Scarif', 'Bespin', 'Cantonica', 'DQar', 'Dantooine', 'Atollon', 'Jedha', 'Corellia', 'Coruscant']
 grafo.add_nodes_from(planetas)
 
-#agregamos las aristas con el metodo add_edge()
-grafo.add_edge('Alderaan', 'Endor')
+#agregamos 4 aristar por cada planeta
+def agregar_aristas(grafo, planetas):
+    for planeta in planetas:
+        for i in range(4):
+            planeta_aleatorio = random.choice([p for p in planetas if p != planeta])
+            grafo.add_edge(planeta, planeta_aleatorio)
+    return grafo
 
-
-
+#algoritmo de Kruskal
