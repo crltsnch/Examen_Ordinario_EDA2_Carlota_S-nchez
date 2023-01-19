a0c2a8d939b18cfaae172ffeb703e405ac5604ba
crltsnch <crltsnch@myuax.com>
2023-01-19 20:13:07 +0100

    Ej 7. funcion buscar

--- ejercicio 7/ejercicio 7.py
+++ ejercicio 7/ejercicio 7.py
@@ -21,5 +21,28 @@ def insertar_nodo(lista_nodos, nodo):
 
     return lista_nodos
 
-def crear_arbol(lista_nodos):
-    
+def crear_arbol(simbolo, freq):
+    nodos = []
+
+    for i in range(len(simbolo)):
+        nodos.append(NodoArbol(simbolo[i], freq[i]))
+    nodos = ordenar_nodos(nodos)
+
+    while len(nodos) > 1:
+        nodo = NodoArbol('XX', nodos[0].freq + nodos[1].freq)  #nodo padre con la suma de las frecuencias de los dos primeros nodos
+        nodo.izq = nodos[0] #el primer nodo de la lista pasa a ser el hijo izquierdo del nodo padre
+        nodo.izq.padre = nodo
+        nodo.der = nodos[1] #el segundo de la lista pasa a ser el nodo dercho del padre
+        nodo.der.padre = nodo
+        nodos = insertar_nodo(nodos, nodo)
+        nodos.pop(0) #eliminamos primer nodo
+        nodos.pop(1) #eliminamos segundo nodo
+
+    return nodos[0] #devolvemos el nodo raiz
+
+def buscar(raiz, clave):
+    p = 0
+    if p is not None:
+        if raiz.simbolo == clave:
+            p = raiz
+        elif p is None:
