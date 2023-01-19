ebd33405e210deeee5e9d5cd80a0277b2ba627d3
crltsnch <crltsnch@myuax.com>
2023-01-19 21:31:00 +0100

    ej 8. añadimos aristas

--- ejercicio 8/ej8.py
+++ ejercicio 8/ej8.py
@@ -4,4 +4,8 @@ grafo = nx.Graph()   #grafo vacío
 planetas = ['Alderaan', 'Endor', 'Dagobah', 'Hoth', 'Tatooine', 'Kamino', 'Naboo', 'Mustafar', 'Scarif', 'Bespin', 'Cantonica', 'DQar', 'Dantooine', 'Atollon', 'Jedha', 'Corellia', 'Coruscant']
 grafo.add_nodes_from(planetas)
 
+#agregamos las aristas con el metodo add_edge()
+grafo.add_edge('Alderaan', 'Endor')
+
+
 
