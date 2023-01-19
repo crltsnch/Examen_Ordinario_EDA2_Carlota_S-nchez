fdf6c4c4035ba9902e812b2292746a146d33fe2f
crltsnch <crltsnch@myuax.com>
2023-01-20 00:46:33 +0100

    Ej 6

--- ejercicio 6/ej 6.py
+++ ejercicio 6/ej 6.py
@@ -20,12 +20,35 @@ listaLegiones = ['FL', 'TF', 'TK', 'CT', 'FN', 'FQ']
 for i in range(2000):
     numRandom = random.randint(0, 5)
     listaNums = []
-    for i in range(4):
-        listaNums.append(random.randint(0, 9))
-
+    for j in range(4):
+        listaNums.append(str(random.randint(0, 9)))
     nums = ''.join(listaNums)
+    tresDigitos = "".join(listaNums[1:])
     codLegion = listaLegiones[numRandom]
-    Stormtrooper = Stormtrooper(codLegion + '-' + nums, 7)
-    print(Stormtrooper)
+    stormtrooper = Stormtrooper(codLegion + '-' + nums, 7)
+    if (hashTresDigitos.get(tresDigitos)):
+        hashTresDigitos[tresDigitos][codLegion] = stormtrooper
+    else:
+        hashTresDigitos[tresDigitos] = {}
+        hashTresDigitos[tresDigitos][codLegion] =stormtrooper
 
+s = Stormtrooper('FN-2187', 7)
+hashTresDigitos['187']['FN'] = s
 
+if (hashTresDigitos.get('187').get('FN').nombre == 'FN-2187'):
+    print('El trooper FN-2187 es el mismo que el FN-2187')
+
+misionDesalto = []
+misionExploracion = []
+
+if (hashTresDigitos.get('781')):
+    for i in hashTresDigitos.get('781').keys():
+        misionDesalto.append(i)
+if (hashTresDigitos.get('537')):
+    for i in hashTresDigitos.get('537').keys():
+        misionExploracion.append(i)
+
+print('Mision desalto')
+print(misionDesalto)
+print('Mision Exploracion')
+print(misionExploracion)
