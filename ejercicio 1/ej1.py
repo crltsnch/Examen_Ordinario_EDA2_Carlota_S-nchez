aa7e6152cac695f43c35716816b278ccf5572063
crltsnch <crltsnch@myuax.com>
2023-01-19 22:54:07 +0100

    Ej 1. funcion clasificacion

--- ejercicio 1/ej1.py
+++ ejercicio 1/ej1.py
@@ -2,3 +2,6 @@ class Stormtroopers:
     def __init__(self, nombre, rango):
         self.nombre = nombre
         self.rango = rango
+        print('Creado con éxito')
+    def clasificacion(self):
+        
