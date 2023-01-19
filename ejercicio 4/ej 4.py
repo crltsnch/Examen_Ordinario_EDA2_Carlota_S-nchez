096dad84a7eb8fdc582f9856e902f981878ae064
crltsnch <crltsnch@myuax.com>
2023-01-19 23:52:52 +0100

    Ej 3. acabado con error

--- ejercicio 4/ej 4.py
+++ ejercicio 4/ej 4.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 class ArtefactosValiosos:
     def __init__(self, nombre, peso, precio, fechaCaducidad):
         self.nombre = nombre
@@ -8,17 +7,5 @@ class ArtefactosValiosos:
         print('Se ha creado un artefacto valioso')
     def __str__(self):
         return 'Nombre: ' + self.nombre + ' Peso: ' + str(self.peso) + ' Precio: ' + str(self.precio) + ' Fecha de caducidad: ' + str(self.fechaCaducidad)
-
-def sortByDate(elem):
-    return datetime.strptime(elem.fechaCaducidad, '%m/%d/%Y')
-
-lista = [ArtefactosValiosos('Diamante', 50, 500, '08/31/2024'),
-ArtefactosValiosos('Esmeralda', 50, 500, '07/28/2023'),
-ArtefactosValiosos('Perla', 50, 500, '09/30/2025')]
-lista.sort(key=sortByDate)
-for stormtrooper in lista:
-    print(stormtrooper)
-
-print('Precio cambiado')
-lista[0].precio = 400
-print(lista[0]) 
+    
+def usarFuerza(m)
