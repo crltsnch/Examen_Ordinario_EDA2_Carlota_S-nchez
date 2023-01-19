7acffaa8d4571e793ed68ef6b0e190b04b574d1a
crltsnch <crltsnch@myuax.com>
2023-01-19 23:29:16 +0100

    Ej 3. constructor y str

--- ejercicio 3/ej3.py
+++ ejercicio 3/ej3.py
@@ -2,4 +2,10 @@ from datetime import datetime
 class ArtefactosValiosos:
     def __init__(self, nombre, peso, precio, fechaCaducidad):
         self.nombre = nombre
-        self.peso =
+        self.peso = peso
+        self.precio = precio
+        self.fechaCaducidad = fechaCaducidad
+        print('Se ha creado un artefacto valioso')
+    def __str__(self):
+        return 'Nombre: ' + self.nombre + ' Peso: ' + str(self.peso) + ' Precio: ' + str(self.precio) + ' Fecha de caducidad: ' + str(self.fechaCaducidad)
+
